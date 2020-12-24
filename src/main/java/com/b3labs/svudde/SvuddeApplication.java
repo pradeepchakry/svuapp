a5f25a817fdefb7f380e477c0d5372ba7ce56c83
discovery <discovery@discovery.discovery>
2020-12-24 17:52:16 +0530

    initial commit

---
+++ src/main/java/com/b3labs/svudde/SvuddeApplication.java
@@ -0,0 +1,13 @@
+package com.b3labs.svudde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvuddeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvuddeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/b3labs/svudde/SvuddeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b3labs.svudde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvuddeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
