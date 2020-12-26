49dbd100310b241629d191df0f5c05c6e833d5e0
root <root@discovery.discovery>
2020-12-26 16:28:33 +0530

    Created model classes

---
+++ src/main/java/com/b3labs/svudde/springboot/modal/Religion.java
@@ -0,0 +1,99 @@
+package com.b3labs.svudde.springboot.modal;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name = "religion")
+public class Religion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, name = "re_id")
+    private Integer id;
+
+    @Column(nullable = false, name = "religion")
+    private String religion;
+
+    @Column(name = "created_on")
+    private Date createdOn;
+
+    @Column(name = "created_by")
+    private Integer createdBy;
+
+    @Column(name = "modified_on")
+    private Date modifiedOn;
+
+    @Column(name = "modified_by")
+    private Integer modifiedBy;
+
+    @Column(nullable = false, columnDefinition = "TINYINT(1)", name = "is_active")
+    private boolean isActive;
+
+    @Column(nullable = false, columnDefinition = "TINYINT(1)", name = "is_delete")
+    private boolean is_delete;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getReligion() {
+        return religion;
+    }
+
+    public void setReligion(String religion) {
+        this.religion = religion;
+    }
+
+    public Date getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(Date createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public Integer getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(Integer createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getModifiedOn() {
+        return modifiedOn;
+    }
+
+    public void setModifiedOn(Date modifiedOn) {
+        this.modifiedOn = modifiedOn;
+    }
+
+    public Integer getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(Integer modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public boolean isIs_delete() {
+        return is_delete;
+    }
+
+    public void setIs_delete(boolean is_delete) {
+        this.is_delete = is_delete;
+    }
+}
